fd9cbaa959c9d9bfe747d4b83267e20f0ae8bf7e
drincastX <drincast@gmail.com>
2017-12-16 13:02:18 -0500

    fin guía de inicio oficial, se agrega componentes fetch para llamados get de api rest o datos json

--- App.js
+++ App.js
@@ -7,8 +7,9 @@
 import React, { Component } from 'react';
 //import { AppRegistry, Platform, StyleSheet, Text, View, Image} from 'react-native';
 import { estilosApp } from './styles/styles.js';
-import { AppRegistry, Platform, Text, View, Image } from 'react-native';
+import { AppRegistry, Image, Platform, ScrollView, Text, View } from 'react-native';
 import { Greeting, Blink, LineaCuadros, PizzaTranslator, BotonEspecial } from './componentes/componentes';
+import { ApiFacebookMoviesJSON, ApiHeroesJSON, ApiHeroes2JSON } from './componentes/componentes';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -25,40 +26,45 @@ export default class App extends Component<{}> {
       uri: 'https://orig00.deviantart.net/da0d/f/2012/354/8/f/celular_png_by_loveofparis-d5om4gr.png'
     };
     return (
-      <View style={estilosApp.container}>
-        <Text style={estilosApp.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={estilosApp.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={estilosApp.instructions}>
-          {instructions}
-        </Text>
-        <View style={estilosApp.imagen}>
-          <Image source={pic} style={{width: 193, height: 110}} />
-        </View>
+      <ScrollView>
+        <View style={estilosApp.container}>
+          <Text style={estilosApp.welcome}>
+            Welcome to React Native!
+          </Text>
+          <Text style={estilosApp.instructions}>
+            To get started, edit App.js
+          </Text>
+          <Text style={estilosApp.instructions}>
+            {instructions}
+          </Text>
+          <View style={estilosApp.imagen}>
+            <Image source={pic} style={{width: 193, height: 110}} />
+          </View>
 
-        <View style={estilosApp.greetings}>
-          <Greeting name='Rexxar' styles={{color: 'blue'}} />
-          <Greeting name='Jaina' />
-          <Greeting name='Valeera' />
-        </View>
+          <View style={estilosApp.greetings}>
+            <Greeting name='Rexxar' styles={{color: 'blue'}} />
+            <Greeting name='Jaina' />
+            <Greeting name='Valeera' />
+          </View>
 
-        <View style={estilosApp.blink}>
-          <Blink text='BLINK !!!!!' />
-          <Blink text='-----------' />
-        </View>
+          <View style={estilosApp.blink}>
+            <Blink text='BLINK !!!!!' />
+            <Blink text='-----------' />
+          </View>
 
-        <View style={estilosApp.textos}>
-          <Text style={estilosApp.red}>Prueba style</Text>
-          <Text style={estilosApp.bigblue}>Prueba style</Text>
-        </View>
+          <View style={estilosApp.textos}>
+            <Text style={estilosApp.red}>Prueba style</Text>
+            <Text style={estilosApp.bigblue}>Prueba style</Text>
+          </View>
 
-        <BotonEspecial />
-        <PizzaTranslator />
-        <LineaCuadros></LineaCuadros>
-      </View>
+          <ApiHeroes2JSON />
+          <ApiHeroesJSON />
+          <ApiFacebookMoviesJSON />
+          <BotonEspecial />
+          <PizzaTranslator />
+          <LineaCuadros></LineaCuadros>
+        </View>
+      </ScrollView>
     );
   }
 }
